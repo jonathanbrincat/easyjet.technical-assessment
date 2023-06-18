db51739c4aaea2933e876a1d267d9968bc54b4da
jonathan <jonathan@pix8.co.uk>
2023-06-18 17:40:33 +0100

    skeletal of fizz buzz component laid out with necessary ui to make this shizzle happen

---
+++ src/vue/components/FizzBuzz.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="component__fizz-buzz">
+    <p>
+      <label>
+        <span>From</span>
+        <input type="number" v-model="state.start" />
+      </label>
+
+      <label>
+        <span>To</span>
+        <input type="number" v-model="state.end" />
+      </label>
+    </p>
+
+    <ul>
+      <li v-for="(number, index) in range" :key="index">
+        {{ number.value }} :: {{ number.verdict }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue'
+
+const state = reactive({
+  start: 0,
+  end: 0,
+})
+
+const range = computed(() => {
+  const collection = [];
+
+  for (let i = state.start; i <= state.end; i++) {
+    collection.push({
+      value: i,
+      verdict: this.fizzbuzz(i),
+    });
+  }
+
+  return collection
+})
+
+</script>
--- src/vue/views/FizzBuzzView.vue
+++ src/vue/views/FizzBuzzView.vue
@@ -1,5 +1,25 @@
+<!--
+===========
+Fizz Buzz
+===========
+
+Write a program that can be given a start and end integer and iterate over every number in that range.
+
+- When the number is a multiple of three mark it as Fizz.
+- When the number is a multiple of five mark it as Buzz.
+- When a number is a multiple of three and five mark it as FizzBuzz.
+
+The program should show the list to the user when it is done.
+-->
+
 <template>
   <main class="view__fizz-buzz">
     <h1>Fizz Buzz</h1>
+
+    <FizzBuzz />
   </main>
 </template>
+
+<script setup>
+import FizzBuzz from '../components/FizzBuzz.vue'
+</script>
